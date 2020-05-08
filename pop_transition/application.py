d7c7028a643c43595b541a210e16d0759dc966f0
Ian Santopietro <ian@system76.com>
2020-05-08 15:47:24 -0600

    fix(testing): Add more concise testing method

--- pop_transition/application.py
+++ pop_transition/application.py
@@ -28,7 +28,8 @@ from .package import Package
 class Application(Gtk.Application):
     """ Application class"""
 
-    def __init__(self):
+    def __init__(self, app_list):
+        self.app_list = app_list
         super().__init__(application_id='org.pop-os.transition',
                          flags=Gio.ApplicationFlags.FLAGS_NONE)
     
@@ -42,46 +43,21 @@ class Application(Gtk.Application):
         """ Connect signals to their functionality."""
         for button in [window.headerbar.cancel_button,
                        window.headerbar.dismiss_button,
-                       window.headerbar.close_button]:
+                       window.headerbar.close_button,
+                       window.dismiss_button]:
             button.connect('clicked', self.on_quit_clicked)
     
     def add_testing_data(self, window):
         """ Populate the GUI with test data to test the UI layout."""
-        android_studio = Package(
-            name='Android Studio',
-            version='3.6.3.0',
-            app_id='com.android.studio',
-            deb='android-studio'
-        )
-        window.app_list.add_package(android_studio)
-        peek = Package(
-            name='Peek',
-            version='1.5.1',
-            app_id='com.peak',
-            deb='peek'
-        )
-        window.app_list.add_package(peek)
-        mattermost = Package(
-            name='Mattermost',
-            version='4.4.1',
-            app_id='com.mattermost.desktop',
-            deb='mattermost-desktop'
-        )
-        window.app_list.add_package(mattermost)
-        spotify = Package(
-            name='Spotify',
-            version='1.1.26.501',
-            app_id='com.spotify.desktop',
-            deb='spotify'
-        )
-        window.app_list.add_package(spotify)
-        signal = Package(
-            name='Signal',
-            version='1.33.4',
-            app_id='com.signal.desktop',
-            deb='signal-desktop'
-        )
-        window.app_list.add_package(signal)
+        for app in self.app_list:
+            pkg = Package()
+            pkg.name = self.app_list[app]['name']
+            pkg.version = self.app_list[app]['version']
+            pkg.icon = self.app_list[app]['icon']
+            pkg.source = 'Flathub'
+            pkg.app_id = self.app_list[app]['id']
+            pkg.deb_package = self.app_list[app]['deb_pkg']
+            window.app_list.add_package(pkg)
 
         window.show_all()
 
