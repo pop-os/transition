8832e9de5d07e9b28b04af7662f72ee6b89db667
Ian Santopietro <ian@system76.com>
2020-05-13 13:02:04 -0600

    feat(polkit): Add PolKit files

---
+++ data/service.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""
+Copyright (c) 2020 Ian Santopietro
+Copyright (c) 2020 System76, Inc.
+All rights reserved.
+
+Permission to use, copy, modify, and/or distribute this software for any purpose
+with or without fee is hereby granted, provided that the above copyright notice 
+and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH 
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND 
+FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, 
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS 
+OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER 
+TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF 
+THIS SOFTWARE.
+
+pop-transition - High-permission service
+"""
+
+import dbus
+import dbus.service
+import dbus.mainloop.glib
+import sys
+import time
+import os
+
+from gi.repository import GLib, GObject
+
