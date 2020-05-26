9ee7e0118d043b351ba895afb7a296885273c7d7
Ian Santopietro <ian@system76.com>
2020-05-26 14:05:43 -0600

    refactor(application): Rework this module
    The reworks the module to make the Application class the main class and the Notification version a subclass with the notification behavior present. This removes a lot of previously duplicated code.

--- pop_transition/application.py
+++ pop_transition/application.py
@@ -31,48 +31,18 @@ from .window import Window
 
 _ = gettext.gettext
 
-class Notification(Gtk.Application):
+class Application(Gtk.Application):
     """ Application class"""
 
     def __init__(self, app_list):
         self.app_list = app_list
         super().__init__(application_id='org.pop_os.transition',
-                         flags=Gio.ApplicationFlags.IS_SERVICE)
-    
-    def do_startup(self):
-        showwin_action = Gio.SimpleAction.new('show-window', None)
-        showwin_action.connect('activate', self.show_window)
-        self.add_action(showwin_action)
-        self.show_notification()
-        Gtk.Application.do_startup(self)
+                         flags=Gio.ApplicationFlags.REPLACE)
     
     def do_activate(self):
-        pass
+        self.show_window()
     
-    def show_notification(self):
-        pkgs = []
-        for pkg in self.get_installed_packages():
-            pkgs.append(pkg)
-        
-        if len(pkgs) > 0:
-            print('Showing notification')
-            notification = Gio.Notification.new(_('Transition to Flatpak'))
-            notification.set_body(
-                _(
-                    'A number of debian packages are no longer recieving updates. '
-                    'Please transition these applications to Flatpak to continue '
-                    'receiving updates.'
-                )
-            )
-            icon = Gio.ThemedIcon.new('system-software-update')
-            notification.set_icon(icon)
-            notification.set_default_action('app.show-window')
-            print(notification)
-            self.send_notification('transition-ready', notification)
-        else:
-            self.quit()
-    
-    def show_window(self, action, data=None):
+    def show_window(self):
         print('showing window')
         self.withdraw_notification('transition-ready')
         self.window = Window(self)
@@ -110,10 +80,9 @@ class Notification(Gtk.Application):
                 remove_debs.append(package.deb_package)
             else:
                 package.spinner.stop()
-                package.status()
+                package.status = 'Removed'
         
         apt.remove_debs(remove_debs, window)
-
     
     def on_install_clicked(self, button, window, data=None):
         print('Install clicked')
@@ -128,9 +97,8 @@ class Notification(Gtk.Application):
                 install_flatpaks.append(package)
             else:
                 package.spinner.stop()
-                package.status = ''
+                package.status = 'Installed'
         flatpak.install_flatpaks(install_flatpaks, window)
-        
     
     def get_installed_packages(self):
         """ Yield a list of installed Packages."""
@@ -149,68 +117,44 @@ class Notification(Gtk.Application):
         """ Clicked signal handler for the various 'quit' buttons."""
         self.quit()
 
-class Application(Gtk.Application):
-    """ Application class"""
+class Notification(Application):
+    """ Application class, with notification"""
 
     def __init__(self, app_list):
         self.app_list = app_list
-        super().__init__(application_id='org.pop_os.transition',
-                         flags=Gio.ApplicationFlags.REPLACE)
+        Gtk.Application.__init__(self, application_id='org.pop_os.transition',
+                         flags=Gio.ApplicationFlags.IS_SERVICE)
+    
+    def do_startup(self):
+        showwin_action = Gio.SimpleAction.new('show-window', None)
+        showwin_action.connect('activate', self.show_window)
+        self.add_action(showwin_action)
+        self.show_notification()
+        Gtk.Application.do_startup(self)
     
     def do_activate(self):
-        self.show_window()
+        pass
     
-    def show_window(self):
-        print('showing window')
-        self.withdraw_notification('transition-ready')
-        self.window = Window(self)
-        self.connect_signals(self.window)
-        for package in self.get_installed_packages():
-                self.window.app_list.add_package(package)
-        self.window.show_all()
-    
-    def connect_signals(self, window):
-        """ Connect signals to their functionality."""
-        for button in [window.headerbar.cancel_button,
-                       window.headerbar.dismiss_button,
-                       window.headerbar.close_button,
-                       window.dismiss_button]:
-            button.connect('clicked', self.on_quit_clicked)
+    def show_notification(self):
+        pkgs = []
+        for pkg in self.get_installed_packages():
+            pkgs.append(pkg)
         
-        window.headerbar.install_button.connect(
-            'clicked', self.on_install_clicked, window
-        )
-    
-    def on_install_clicked(self, button, window, data=None):
-        print('Install clicked')
-        window.set_buttons_sensitive(False)
-        install_flatpaks = []
-        
-        for package in window.app_list.packages:
-            package.spinner.start()
-            package.status = 'Checking'
-            if package.checkbox.get_active():
-                package.status = 'Installing'
-                install_flatpaks.append(package)
-            else:
-                package.spinner.stop()
-                package.status = ''
-        flatpak.install_flatpaks(install_flatpaks, window)
-        
-    
-    def get_installed_packages(self):
-        """ Yield a list of installed Packages."""
-        for app in self.app_list:
-            pkg = Package()
-            pkg.name = self.app_list[app]['name']
-            pkg.version = self.app_list[app]['version']
-            pkg.icon = self.app_list[app]['icon']
-            pkg.source = 'Flathub'
-            pkg.app_id = self.app_list[app]['id']
-            pkg.deb_package = self.app_list[app]['deb_pkg']
-            if pkg.installed:
-                yield pkg
-
-    def on_quit_clicked(self, button, data=None):
-        """ Clicked signal handler for the various 'quit' buttons."""
-        self.quit()
+        if len(pkgs) > 0:
+            print('Showing notification')
+            notification = Gio.Notification.new(_('Transition to Flatpak'))
+            notification.set_body(
+                _(
+                    'A number of debian packages are no longer recieving updates. '
+                    'Please transition these applications to Flatpak to continue '
+                    'receiving updates.'
+                )
+            )
+            icon = Gio.ThemedIcon.new('system-software-update')
+            notification.set_icon(icon)
+            notification.set_default_action('app.show-window')
+            print(notification)
+            self.send_notification('transition-ready', notification)
+        else:
+            self.quit()
+            
